e37194a7c996f7ffb35ab58d764c7f7deb3fb725
Yusuf Ansari <yusssuf0786@gmail.com>
2024-05-27 00:30:57 +0530

    package setup

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const { default: axios } = require("axios");
+
+const url = "http://192.168.1.9:1000/apitest/admin/admin_GetComplainList.php"
+const url2 = "http://octaprosolution.com/apitest/admin/admin_GetComplainList.php"
+
+const options = {
+    // method:"POST",
+    // headers: {
+    //     "Content-type": "application/json"
+    // },
+    // body: JSON.stringify({
+        test: "test"
+    // })
+}
+
+// axios.post(url, options)
+// .then(response => console.log(response))
+// .catch(error => console.log(error.message))
+
+// fetch(url2).then(response => response.json()).then(json => console.log(json))
